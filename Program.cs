5e8c428904487c96b57dae35e578e43002f40e66
batston <gbatston@gmail.com>
2022-08-05 15:20:11 +0300

    Создаем функцию для подсчета количества строк, длина которых меньше либо равна 3.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
 ﻿
 string[] str = {"1234", "567", "asd", "fghjl", "sport", "ref123", "9", "345", "javascript", "pyton", "tre"};
 
+
+
+//Создаем функцию для подсчета количества строк, длина которых меньше либо равна 3.
+int AmountElementsArray(string[] strings)
+{
+    int count = 0;
+    for (int i = 0; i < strings.Length; i++)
+    {
+        if (strings[i].Length <= 3)
+        {
+            count++;
+        }
+    }
+    return count;
+}
